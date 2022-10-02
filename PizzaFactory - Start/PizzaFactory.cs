9da6ae6dde4b3fa46de89eff0e080bde36b04428
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-10-02 20:04:04 +0200

    Add project files.

---
+++ PizzaFactory - Start/PizzaFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp
+{
+    public class PizzaFactory : IPizzaFactory
+    {
+
+
+        public AbstractPizza MakePizza(string[] additionals)
+        {
+            AbstractPizza pizza = new PizzaMargaritta(); ;
+            return pizza;
+        }
+
+        private AbstractPizza AddIngredients(string ingredients, AbstractPizza pizza)
+        {
+            return null;
+        }
+    }
+}
---
+++ PizzaFactory - Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaFactory___Start
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
